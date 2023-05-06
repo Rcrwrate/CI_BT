40f51512cb49495ac564069f79d1aeda959b1d35
琪露诺酱 <46920034+Rcrwrate@users.noreply.github.com>
2023-05-06 08:26:53 +0000

    fix

--- unzip.sh
+++ unzip.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
+# 解压缩函数
+function extract_archive() {
+  local archive="$1"
+  local destination="$2"
+
+  if [[ "$archive" == *.zip ]]; then
+    unzip -n "$archive" -d "$destination"
+  elif [[ "$archive" == *.7z ]]; then
+    7z x "$archive" -o"$destination" -y
+  fi
+}
+
+# 遍历文件夹中的所有文件
+function process_files() {
+  local dir="$1"
+
+  for file in "$dir"/*; do
+    # 如果是压缩包则解压
+    if [[ "$file" == *.zip || "$file" == *.7z ]]; then
+      # 创建目标文件夹
+      filename=$(basename "$file" | cut -f 1 -d '.')
+      mkdir "$dir/$filename"
+      # 解压到目标文件夹
+      extract_archive "$file" "$dir/$filename"
+      # 递归处理目标文件夹下的文件
+      process_files "$dir/$filename"
+    fi
+  done
+}
+
 # 指定要遍历的文件夹
 dir="./image"
 
-# 遍历文件夹中的所有文件
-for file in "$dir"/*
-do
-  # 判断文件是否为压缩包（zip 或 7z）
-  if [[ "$file" == *.zip || "$file" == *.7z ]]
-  then
-    # 解压文件到以文件名命名的文件夹中
-    filename=$(basename "$file" | cut -f 1 -d '.')
-    mkdir "$dir/$filename"
-    if [[ "$file" == *.zip ]]
-    then
-      unzip -n "$file" -d "$dir/$filename"
-    else
-      7z x "$file" -o"$dir/$filename" -y
-    fi
-  fi
-done
+# 调用函数处理文件夹中的压缩包
+process_files "$dir"
